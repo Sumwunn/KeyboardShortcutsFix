11c2cd7a7073b90833da58a0fbde29e9cd95d697
Sumwunn <0x77VA@gmail.com>
2017-10-08 13:44:24 -0400

    Code cleanup. 3 new ini options.

--- KeyboardShortcutsFix/KeyboardShortcutsFix.cpp
+++ KeyboardShortcutsFix/KeyboardShortcutsFix.cpp
@@ -26,97 +26,193 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
 #include <string>
 
 // Defined functions.
-// ASM.
+// ASM
 extern "C" void* BinSearch(void* Search, int SearchLength, unsigned char* Bytes, int BytesLength, int AddMod, int SubMod);
 // Work around because my ASM function GetTextSectionData has multiple return value data types.
 extern "C" void* GetTextSectionAddr(HMODULE Module, int DataType);
 extern "C" int GetTextSectionSize(HMODULE Module, int DataType);
+// C++
+int BinPatch(HMODULE hModule, unsigned char* BytesToFind, int BytesToFindSize, unsigned char* BytesPatch, int BytesPatchSize, int AddressModifierAdd, int AddressModifierSub);
 
 // Return values
+// 0 = Patching failed, bytes not found.
+// 1 = Patching successful, bytes found.
 // -1 = Process is NOT expected target.
 // -2 = Log file creation failed.
 
 extern "C" __declspec(dllexport) int Setup(int ScriptExtenderType)
+// ScriptExtenderType
+// 1 = (reserved). 2 = SKSE64.
 {
 	LPCTSTR ExpectedProcess01 = L"SkyrimSE.exe";
-	// These bytes will land us exactly at where IDirectInputDevice8::SetCooperativeLevel's dwFlags is.
-	unsigned char BytesToFind01[] = { 0x41, 0xB8, 0x15, 0x00, 0x00, 0x00, 0x48, 0x8B, 0xD0, 0xFF, 0x53, 0x68 };
+	// These bytes will land us exactly at where IDirectInputDevice8::SetCooperativeLevel's is.
+	unsigned char BytesToFind01_01[] = { 0x41, 0xB8, 0x15, 0x00, 0x00, 0x00, 0x48, 0x8B, 0xD0, 0xFF, 0x53, 0x68 };
 	// This is what we patch it with (check notes.txt).
-	unsigned char BytesPatch01[] = { 0x48, 0x31, 0xD2 };
+	unsigned char BytesPatch01_01[] = { 0x48, 0x31, 0xD2 }; // hwnd.
+	unsigned char BytesPatch01_02[] = { 0x41, 0xB8, 0x0D }; // dwFlags.
+	// We need to go back X bytes so we land at the right address.
+	int AddressModifierSub01_01 = 0x6; // hwnd.
 
-	// Pointers.
-	unsigned char *BytesToFind = NULL;
-	unsigned char *BytesPatch = NULL;
-	int BytesToFindSize = NULL;
-	int BytesPatchSize = NULL;
+	//////// Setup Part 1 - Config ////////
 
+	TCHAR ConfigFilePath[MAX_PATH];
+	int iEnableLogging = 1;
+	// 0 = Disable.
+	// 1 = Enable.
+	int iIgnoreExpectedProcessName = 0;
+	// 0 = Expected process name detection enabled.
+	// 1 = Ignore SkyrimSE.exe name detection. Allows mod to work regardless of EXE name.
+	int iPatchMethod = 1;
+	// 1 = hwnd.
+	// 2 = dwFlags.
+
+	// Get config path.
+	GetCurrentDirectory(MAX_PATH, ConfigFilePath);
+	if (ScriptExtenderType == 2) // SKSE64 path.
+	{
+		_tcscat_s(ConfigFilePath, MAX_PATH, L"\\Data\\SKSE\\Plugins\\KeyboardShortcutsFix.ini");
+	}
+	else // Dll loader path.
+	{
+		_tcscat_s(ConfigFilePath, MAX_PATH, L"\\Data\\Plugins\\Sumwunn\\KeyboardShortcutsFix.ini");
+	}
+	// Get config settings.
+	iEnableLogging = GetPrivateProfileInt(L"General", L"iEnableLogging", 1, ConfigFilePath);
+	iIgnoreExpectedProcessName = GetPrivateProfileInt(L"General", L"iIgnoreExpectedProcessName", 0, ConfigFilePath);
+	iPatchMethod = GetPrivateProfileInt(L"General", L"iPatchMethod", 1, ConfigFilePath);
+
+	// Checking for incorrect values.
+	if (iEnableLogging < 0 || iEnableLogging > 1)
+	{
+		iEnableLogging = 1;
+	}
+	if (iIgnoreExpectedProcessName < 0 || iIgnoreExpectedProcessName > 1)
+	{
+		iIgnoreExpectedProcessName = 0;
+	}
+	if (iPatchMethod < 0 || iPatchMethod > 2)
+	{
+		iPatchMethod = 1;
+	}
+
+	// Misc.
+	HMODULE hModule = NULL;
+	std::ofstream LogFileHandle;
+
+	//////// Setup Part 2 - Addresses & Logging ////////
+
+	if (iEnableLogging == 1)
+	{
+		// Open up fresh log file.
+		if (ScriptExtenderType == 2) // SKSE64 path.
+		{
+			LogFileHandle.open(L"Data\\SKSE\\Plugins\\KeyboardShortcutsFix.log");
+		}
+		else // Dll loader path.
+		{
+			LogFileHandle.open(L"Data\\Plugins\\Sumwunn\\KeyboardShortcutsFix.log");
+		}
+
+		// Log file creation failed.
+		if (!LogFileHandle)
+		{
+			return -2;
+		}
+	}
+
+	// Skyrim SE.
+	if (iIgnoreExpectedProcessName == 1)
+	{
+		hModule = GetModuleHandle(NULL);
+	}
+	else
+	{
+		hModule = GetModuleHandle(ExpectedProcess01);
+	}
+	// Did we get hModule?
+	if (hModule != NULL)
+	{
+		// Find bytes and patch them.
+		int PatchingResult = NULL;
+		// hwmd method.
+		if (iPatchMethod = 1) 
+		{
+			PatchingResult = BinPatch(hModule, BytesToFind01_01, sizeof BytesToFind01_01, BytesPatch01_01, sizeof BytesPatch01_01, AddressModifierSub01_01, NULL);
+		}
+		// dwFlags method.
+		else if (iPatchMethod = 2)
+		{
+			PatchingResult = BinPatch(hModule, BytesToFind01_01, sizeof BytesToFind01_01, BytesPatch01_02, sizeof BytesPatch01_02, NULL, NULL);
+		}
+		// Compare patching result and write result into log file.
+		if (PatchingResult == 0)
+		{
+			if (iEnableLogging == 1)
+			{
+				// Bytes not found!
+				// Log message.
+				LogFileHandle << "NO" << std::endl;
+				// Cleanup.
+				LogFileHandle.close();
+			}
+			return 0;
+		}
+		else
+		{
+			if (iEnableLogging == 1)
+			{
+				// Bytes found!
+				// Log message.
+				LogFileHandle << "YES" << std::endl;
+				// Cleanup.
+				LogFileHandle.close();
+			}
+			return 1;
+		}
+	}
+
+	if (iEnableLogging == 1)
+	{
+		// Process not found.
+		// Cleanup.
+		// Log message.
+		LogFileHandle << "SkyrimSE.exe not detected." << std::endl;
+		LogFileHandle.close();
+	}
+	return -1;
+}
+
+int BinPatch(HMODULE hModule, unsigned char* BytesToFind, int BytesToFindSize, unsigned char* BytesPatch, int BytesPatchSize, int AddressModifierAdd, int AddressModifierSub) // BinSearch + MEMCPY patching.
+{
 	// The address we get from GetTextSectionAddr.
 	void* SearchAddress = (void*)NULL;
 	// The size too.
 	int SearchSize = NULL;
 	// The address we get from BinSearch.
 	void* PatchAddress = (void*)NULL;
-	// We need to go back X bytes so we land at the right address.
-	int PatchAddressModifier = NULL;
-	int PatchAddressModifier01 = 0x6; // Skyrim SE.
-
 	// Misc.
-	DWORD OldVP = NULL;
-
-	//////// Setup Part 1 - Addresses & Logging ////////
-
-	// Get module of target to writes hooks to.
-	// SkyrimSE.exe
-	HMODULE TargetModule = GetModuleHandle(ExpectedProcess01);
-	if (TargetModule != NULL) 
-	{
-		PatchAddressModifier = PatchAddressModifier01;
-		BytesToFind = BytesToFind01;
-		BytesPatch = BytesPatch01;
-		BytesToFindSize = sizeof BytesToFind01;
-		BytesPatchSize = sizeof BytesPatch01;
-	}
-
-	// Open up fresh log file.
-	std::ofstream LogFileHandle;
-	// Open up fresh log file.
-	if (ScriptExtenderType == 2) // SKSE64 path.
-	{
-		LogFileHandle.open(L"Data\\SKSE\\Plugins\\KeyboardShortcutsFix.log");
-	}
-	else // Dll loader path.
-	{
-		LogFileHandle.open(L"Data\\Plugins\\Sumwunn\\KeyboardShortcutsFix.log");
-	}
+	DWORD lpflOldProtect = NULL;
 
 	// Get size and address of ExpectedProcess's .text section.
-	SearchSize = GetTextSectionSize(TargetModule, 1);
-	SearchAddress = GetTextSectionAddr(TargetModule, 2);
-
+	SearchSize = GetTextSectionSize(hModule, 1);
+	SearchAddress = GetTextSectionAddr(hModule, 2);
 	// Get address and patch it.
-	PatchAddress = BinSearch(SearchAddress, SearchSize, BytesToFind, BytesToFindSize, PatchAddressModifier01, NULL);
-	// Bytes not found!
-	if (PatchAddress == NULL) 
+	PatchAddress = BinSearch(SearchAddress, SearchSize, BytesToFind, BytesToFindSize, AddressModifierAdd, AddressModifierSub);
+	if (PatchAddress == NULL)
 	{
-		// Log message.
-		LogFileHandle << "NO" << std::endl;
+		// Bytes not found.
+		return 0;
 	}
 	// Bytes found!
-	else 
+	else
 	{
 		// Patch it! (with NOPS)
-		VirtualProtect(PatchAddress, BytesPatchSize, PAGE_EXECUTE_READWRITE, &OldVP);
+		VirtualProtect(PatchAddress, BytesPatchSize, PAGE_EXECUTE_READWRITE, &lpflOldProtect);
 		memcpy(PatchAddress, BytesPatch, BytesPatchSize);
-		VirtualProtect(PatchAddress, BytesPatchSize, OldVP, &OldVP);
-		// Log message.
-		LogFileHandle << "YES" << std::endl;
-		// Cleanup.
-		LogFileHandle.close();
+		VirtualProtect(PatchAddress, BytesPatchSize, lpflOldProtect, &lpflOldProtect);
 		return 1;
 	}
 
-	// Cleanup.
-	LogFileHandle.close();
 	return 0;
 }
 
